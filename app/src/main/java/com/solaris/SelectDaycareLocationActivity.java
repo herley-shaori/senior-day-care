776874ca3efc83c724fc610cd1006098acd80c6f
herley.shaori1 <herley.shaori@gmail.com>
2021-06-01 23:10:25 +0700

    Set initial display of daycare location.

--- app/src/main/java/com/solaris/SelectDaycareLocationActivity.java
+++ app/src/main/java/com/solaris/SelectDaycareLocationActivity.java
@@ -3,6 +3,10 @@ package com.solaris;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
 
 public class SelectDaycareLocationActivity extends AppCompatActivity {
 
@@ -13,5 +17,35 @@ public class SelectDaycareLocationActivity extends AppCompatActivity {
 
         this.getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         this.getSupportActionBar().setTitle("Back");
+
+        ListView listView = findViewById(R.id.lansiaPlaceSelection);
+        listView.setAdapter(new LansiaPlaceSelectionCustomAdapter());
+    }
+
+    /**
+     * Custom adapter class for lansia.
+     */
+    class LansiaPlaceSelectionCustomAdapter extends BaseAdapter{
+
+        @Override
+        public int getCount() {
+            return 1;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View v = getLayoutInflater().inflate(R.layout.panti_jompo_list,null);
+            return v;
+        }
     }
 }
